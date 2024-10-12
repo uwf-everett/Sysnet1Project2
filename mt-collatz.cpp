e50c880af746c1e823a9d1d0430435e6b7bdda50
Peter Ballard <pjb28@students.uwf.edu>
2024-10-12 14:15:59 -0500

    Removed my bs in mt-collatz so that it works with the command line again

--- mt-collatz.cpp
+++ mt-collatz.cpp
@@ -4,22 +4,12 @@
 int valueBeingComputed = 0;
 
 int main(int argc, char** argv){
-    Collatz c = Collatz(100000, 0);
-
-    /*
     //converts command line arguments to ints
     int n = atoi(argv[1]);
     int t = atoi(argv[2]);
     
     //creates Collatz object
     Collatz c2 = Collatz(n,t);
-    //prints values to make sure they are correct
-    c2.testFunction();
-
-    //make t amount of threads probably via an array and for loop
-    //thread call increment valueBeingComputed so it doesnt work on the same value twice
-    //all the way up to n(use the getN function)
-    */
 
     return 0;
 }
