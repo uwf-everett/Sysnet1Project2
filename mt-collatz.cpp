ee05e2d3bb22007e2eee5107dd0cbf50e569927b
Peter Ballard <pjb28@students.uwf.edu>
2024-10-12 14:24:27 -0500

    Added some to-do to main

--- mt-collatz.cpp
+++ mt-collatz.cpp
@@ -13,3 +13,19 @@ int main(int argc, char** argv){
 
     return 0;
 }
+
+// CODING TO-DO:
+// need to do sterr or something for running time instead of what I did
+// need [-nolock]
+// need a way to write to a file
+// can't compute n values larger than 100,000
+
+
+// NON-CODING TO-DO:
+// description of experiment
+// visualized histogram of stopping times
+// results from experiments including a graph comparing threads vs runtime with and without locks
+// analysis of performance improvements through parallel execution
+// analysis on impact of locks on performance
+    // I already know this greatly lowers performance
+// conclusions from experiments
